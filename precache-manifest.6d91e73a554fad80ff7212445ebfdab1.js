d91dbe9dae7beee7415762539cfe4c6329d84539
pachkovska <pachkovska@gmail.com>
2020-05-01 10:36:06 -0700

    Updates

---
+++ precache-manifest.6d91e73a554fad80ff7212445ebfdab1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59399b6d18701e9a29eeaa357464fff8",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "dfa2c1a813c5d84c9974",
+    "url": "/tic-tac-toe/static/css/main.b1217c70.chunk.css"
+  },
+  {
+    "revision": "c2f9629eaa258f0978e4",
+    "url": "/tic-tac-toe/static/js/2.eea45732.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tic-tac-toe/static/js/2.eea45732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa2c1a813c5d84c9974",
+    "url": "/tic-tac-toe/static/js/main.0657d034.chunk.js"
+  },
+  {
+    "revision": "b86abd17302d45d32434",
+    "url": "/tic-tac-toe/static/js/runtime-main.1fbe6e35.js"
+  }
+]);
