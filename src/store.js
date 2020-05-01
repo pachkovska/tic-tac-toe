680ebdfaf0ed7d1b65f66f575dae3db3d78c1197
pachkovska <pachkovska@gmail.com>
2020-05-01 10:31:34 -0700

    clean store file

--- src/store.js
+++ src/store.js
@@ -1,12 +1,5 @@
-import { applyMiddleware, createStore } from 'redux';
-// import thunk from 'redux-thunk';
+import { createStore } from 'redux';
 import reducer from './reducer';
-// import { composeWithDevTools } from 'redux-devtools-extension';
-
-// const store = createStore(
-//     reducer,
-//     composeWithDevTools(applyMiddleware(thunk)),
-// );
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
